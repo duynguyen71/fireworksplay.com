9076d691111dd3d20101c905b4b198f9d0a8d1c5
Duy Nguyen <khanhduy.dev.bt@gmail.com>
2025-06-29 16:59:16 +0700

    no message

--- src/data/updates/2025-05.js
+++ src/data/updates/2025-05.js
@@ -1,15 +1,5 @@
 const updatesMay2025 = [
-  {
-    version: "Version 2025.6.1",
-    changes: [
-      "Shells: Peony 05, 06; Ghost Shell 23, 24, 25, 26, 27; Palm 06; Strobe 15; Crackle 32; Big Shell 06; Chrysanthemum 19",
-      "Specials: Flammpanzer, Sparkling Dragon, Pipe Ship.",
-      "Props: Disco Ball.",
-      "Fireworks: Pink Pig.",
-      "Map: Isolate Island.",
-      "https://youtu.be/vCZbTBtXM8E",
-    ],
-  },
+
   {
     version: "Version 2025.5.1",
     changes: [
