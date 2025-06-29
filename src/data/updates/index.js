15331f55d8a8d8787b49721e559b3e51d2b769ce
Duy Nguyen <khanhduy.dev.bt@gmail.com>
2025-06-29 22:09:57 +0700

    update ci

--- src/data/updates/index.js
+++ src/data/updates/index.js
@@ -1,9 +1,8 @@
 import updates202506 from './2025-06.js';
 import updates202505 from './2025-05.js';
-import notionUpdates from './notion-latest-updates.json';
+
 
 const allUpdates = [
-  ...notionUpdates,
   ...updates202506,
   ...updates202505
 ];
